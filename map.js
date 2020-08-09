64dbe96bc5796588d29cfb6c3b93e9c4428537ec
fullstackdeveloperkishan <kishanguptanri@gmail.com>
2020-08-09 05:54:55 +0530

    map

--- map.js
+++ map.js
@@ -9,11 +9,12 @@ window.addEventListener("load", async function () {
         center: [80, 23]
     });
 
+    mapArrayData = []
 
-
-    this.loadMap();
-    // await this.updateMap();
-}, false);
+    await this.updateMap();
+    
+    
+}, true);
 
 
 function counter() {
@@ -25,13 +26,7 @@ function counter() {
 
 function loadMap() {
     this.map.on('load', async function () {
-        map.loadImage(
-            'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-            // Add an image to use as a custom marker
-            function (error, image) {
-                if (error) throw error;
-                map.addImage('custom-marker', image);
-
+            this.mapArrayData
                 map.addSource('places', {
                     'type': 'geojson',
                     'data': {
@@ -46,94 +41,6 @@ function loadMap() {
                                 'geometry': {
                                     'type': 'Point',
                                     'coordinates': [-77.038659, 38.931567]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Mad Men Season Five Finale Watch Party</strong><p>Head to Lounge 201 (201 Massachusetts Avenue NE) Sunday for a Mad Men Season Five Finale Watch Party, complete with 60s costume contest, Mad Men trivia, and retro food and drink. 8:00-11:00 p.m. $10 general admission, $20 admission and two hour open bar.</p>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.003168, 38.894651]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Big Backyard Beach Bash and Wine Fest</strong><p>EatBar (2761 Washington Boulevard Arlington VA) is throwing a Big Backyard Beach Bash and Wine Fest on Saturday, serving up conch fritters, fish tacos and crab sliders, and Red Apron hot dogs. 12:00-3:00 p.m. $25.</p>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.090372, 38.881189]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Ballston Arts & Crafts Market</strong><p>The Ballston Arts & Crafts Market sets up shop next to the Ballston metro this Saturday for the first of five dates this summer. Nearly 35 artists and crafters will be on hand selling their wares. 10:00-4:00 p.m.</p>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.111561, 38.882342]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        "<strong>Seersucker Bike Ride and Social</strong><p>Feeling dandy? Get fancy, grab your bike, and take part in this year's Seersucker Social bike ride from Dandies and Quaintrelles. After the ride enjoy a lawn party at Hillwood with jazz, cocktails, paper hat-making, and more. 11:00-7:00 p.m.</p>"
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.052477, 38.943951]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Capital Pride Parade</strong><p>The annual Capital Pride Parade makes its way through Dupont this Saturday. 4:30 p.m. Free.</p>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.043444, 38.909664]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Muhsinah</strong><p>Jazz-influenced hip hop artist Muhsinah plays the Black Cat (1811 14th Street NW) tonight with Exit Clov and Gods’illa. 9:00 p.m. $12.</p>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.031706, 38.914581]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        "<strong>A Little Night Music</strong><p>The Arlington Players' production of Stephen Sondheim's <em>A Little Night Music</em> comes to the Kogod Cradle at The Mead Center for American Theater (1101 6th Street SW) this weekend and next. 8:00 p.m.</p>"
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.020945, 38.878241]
-                                }
-                            },
-                            {
-                                'type': 'Feature',
-                                'properties': {
-                                    'description':
-                                        '<strong>Truckeroo</strong><p>Truckeroo brings dozens of food trucks, live music, and games to half and M Street SE (across from Navy Yard Metro Station) today from 11:00 a.m. to 11:00 p.m.</p>'
-                                },
-                                'geometry': {
-                                    'type': 'Point',
-                                    'coordinates': [-77.007481, 38.876516]
                                 }
                             }
                         ]
@@ -150,8 +57,6 @@ function loadMap() {
                         'icon-allow-overlap': true
                     }
                 });
-            }
-        );
 
         // Create a popup, but don't add it to the map yet.
         popup = new mapboxgl.Popup({
@@ -196,13 +101,32 @@ function loadMap() {
 async function updateMap() {
     // console.log("Updating map with realtime data"
     $.ajax({
-        url: "https://disease.sh/v3/covid-19/countries", success: function (data) {
+        url: "https://disease.sh/v3/covid-19/countries", success: async function (data) {
+            var finalHoverdata = [];
             for (var i = 0; i < data.length; i++) {
                 var latitude = data[i].countryInfo.lat
                 var longitude = data[i].countryInfo.long
                 var mapDeaths = data[i].deaths
-
+                var mapCountry = data[i].country
+                var mapRecovered = data[i].recovered
+                var mapActive = data[i].active
+                var mapTests = data[i].tests
                 var cases = data[i].cases
+                var mapContinents = data[i].continents
+
+                var hoverData = {
+                    latitude: latitude,
+                    longitude: longitude,
+                    mapDeaths: mapDeaths,
+                    mapCountry: mapCountry,
+                    mapRecovered: mapRecovered,
+                    mapActive: mapActive,
+                    mapTests: mapTests,
+                    mapCases: cases,
+                    mapContinents: mapContinents
+                }
+
+                finalHoverdata.push(hoverData)
 
                 if (cases > 5000000) {
                     color = "rgb(20,0,0)"
@@ -226,6 +150,12 @@ async function updateMap() {
 
                 }).setLngLat([longitude, latitude]).addTo(map);
             }
+            // console.log(finalHoverdata);
+            this.mapArrayData = finalHoverdata;
+
+            console.log(this.mapArrayData, "Map array");
+            await loadMap();
+
         }
     });
 
