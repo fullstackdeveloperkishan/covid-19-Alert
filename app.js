9b5045e0c36db8c80ece997519b2a3f367c321e6
Rahul Manas <rahul@getnuo.com>
2020-08-16 01:32:57 +0530

    Minor indent

--- app.js
+++ app.js
@@ -20,71 +20,71 @@ app.use(express.static(__dirname + '/'));
 const url = "https://api.covid19india.org/data.json";
 // const mapUrl = "https://disease.sh/v3/covid-19/countries";
 
-app.get('/teams',(req,res)=>{
+app.get('/teams', (req, res) => {
   res.render('teams');
 });
 
-app.get('/index',(req,res)=>{
+app.get('/index', (req, res) => {
   res.render('index');
 });
 
-app.get('/awareness',(req,res)=>{
+app.get('/awareness', (req, res) => {
   res.render('awareness');
 });
-app.get('/covid',(req,res)=>{
+app.get('/covid', (req, res) => {
   res.render('covid');
 });
 
 //API request
-request({url: url}, (error , response) => {
-    const coronadata = JSON.parse(response.body)
-   //top data total
-     const Confirmed = coronadata['statewise'][0].confirmed.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     const Active =coronadata['statewise'][0].active.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     const Deaths =coronadata['statewise'][0].deaths.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     const Recovered =coronadata['statewise'][0].recovered.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     const LastUpdated = coronadata['statewise'][0].lastupdatedtime.toString()
+request({ url: url }, (error, response) => {
+  const coronadata = JSON.parse(response.body)
+  //top data total
+  const Confirmed = coronadata['statewise'][0].confirmed.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const Active = coronadata['statewise'][0].active.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const Deaths = coronadata['statewise'][0].deaths.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const Recovered = coronadata['statewise'][0].recovered.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const LastUpdated = coronadata['statewise'][0].lastupdatedtime.toString()
 
-    //daily increase
-     const dailyConfirmed =coronadata['statewise'][0].deltaconfirmed.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     const dailyRecovered =coronadata['statewise'][0].deltarecovered.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     const dailyDeaths =coronadata['statewise'][0].deltadeaths.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //daily increase
+  const dailyConfirmed = coronadata['statewise'][0].deltaconfirmed.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const dailyRecovered = coronadata['statewise'][0].deltarecovered.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  const dailyDeaths = coronadata['statewise'][0].deltadeaths.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    
-     //list of covid data code
-     
-     var cdata = coronadata['statewise'];
 
-     for(var i = 0; i < cdata.length; i++) {
-       cdata[i].active = cdata[i].active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-       cdata[i].deaths = cdata[i].deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-       cdata[i].recovered = cdata[i].recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-       cdata[i].confirmed = cdata[i].confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-     }
-    
-    
-     
-    //removing the total data from index[0] and deleting the state unassigned data
-      delete cdata['36'];
-      cdata.shift()
+  //list of covid data code
+
+  var cdata = coronadata['statewise'];
+
+  for (var i = 0; i < cdata.length; i++) {
+    cdata[i].active = cdata[i].active.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    cdata[i].deaths = cdata[i].deaths.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    cdata[i].recovered = cdata[i].recovered.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    cdata[i].confirmed = cdata[i].confirmed.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
 
 
 
-      //chart
-      
+  //removing the total data from index[0] and deleting the state unassigned data
+  delete cdata['36'];
+  cdata.shift()
+
+
+
+  //chart
+
 
   //getting data from server
-  app.get('/', (req,res) => {
-     res.render('index', {
-       totalconfirmed: Confirmed,
-       totalactive: Active,
-       totaldeaths: Deaths,
-       totalrecovered: Recovered,
-       lastUpdated: LastUpdated,
-      
-       //daily increase
+  app.get('/', (req, res) => {
+    res.render('index', {
+      totalconfirmed: Confirmed,
+      totalactive: Active,
+      totaldeaths: Deaths,
+      totalrecovered: Recovered,
+      lastUpdated: LastUpdated,
+
+      //daily increase
       increaseConfirmed: dailyConfirmed,
-      increaseRecovered:  dailyRecovered,
+      increaseRecovered: dailyRecovered,
       increaseDeaths: dailyDeaths,
       //loopdata
       cdata
@@ -92,17 +92,17 @@ request({url: url}, (error , response) => {
   })
 })
 
-      
-        
-  
+
+
+
 //loacal host
 
 const port = process.env.PORT || 3000
 
 reload(app).then(function (reloadReturned) {
-  app.listen(port,() => {
-      console.log("server is working");
-      // console.log(https);
+  app.listen(port, () => {
+    console.log("server is working");
+    // console.log(https);
   })
 }).catch(function (err) {
   console.error('Reload could not start, could not start server/sample app', err)
